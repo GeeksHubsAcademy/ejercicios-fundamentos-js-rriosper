1802d8f583e6b1175a8e5d9e6fd885881168f7a1
Roberto Ríos <roberto.rios.perea@outlook.com>
2018-05-31 18:28:03 +0200

    Ejercicio resuelto | bucles| 5-bucles

--- 3-bucles-avanzado/5-bucles.js
+++ 3-bucles-avanzado/5-bucles.js
@@ -17,8 +17,10 @@ for (let i = 0; i < giveMeIntegerRandom(10); i++) {
 console.log('El array original es:  arr =', arr);
 
 // Aquí tu código
-
-
+let total = 0;
+arr.forEach(n => {
+    total += n
+});
 // Hasta aquí
 console.log('El total nuevo es: ', total);
 
