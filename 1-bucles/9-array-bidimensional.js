784b5ce9e622e8f64adda579c46bb7b7ec2abbba
Roberto Ríos <roberto.rios.perea@outlook.com>
2018-05-31 18:26:30 +0200

    Ejercicio resuelto | bucles | 9-bucle-for

--- 1-bucles/9-array-bidimensional.js
+++ 1-bucles/9-array-bidimensional.js
@@ -1,4 +1,3 @@
-
 // ej 9
 // Rellena la matriz como la siguiente utilizando bucles for anidados
 // let matriz = [
@@ -6,9 +5,14 @@
 //   ['10','11','12'],
 //   ['20','21','22']
 // ];
-
 let matriz = [
     [],
     [],
     []
 ]
+for (let i = 0; i < matriz.length; i++) {
+    for(let x = 0; x < 3; x++){
+        matriz[i][x] = `${i}${x}`;
+    }
+}
+console.log(matriz);
